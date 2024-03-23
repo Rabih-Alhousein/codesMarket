4bb08619cdad5fe39a11a63a5bc682a58b298e18
Rabih ElHoussein <rabih.elhoussein@nascodigital.com>
2024-03-23 03:57:15 +0200

    security + mobile nav + products page + styles fixes

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -1,7 +1,10 @@
+import { NextRequest } from "next/server";
 import { User } from "../payload-types";
 import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
 
-export const getServerSideUser = async (cookies: ReadonlyRequestCookies) => {
+export const getServerSideUser = async (
+  cookies: NextRequest["cookies"] | ReadonlyRequestCookies
+) => {
   const token = cookies.get("payload-token")?.value;
 
   const meRes = await fetch(
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
+import { inferAsyncReturnType } from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import bodyParser from "body-parser";
 import express from "express";
+import { IncomingMessage } from "http";
+import nextBuild from "next/dist/build";
+import path from "path";
 import { getPayloadClient } from "./get-payload";
 import { nextApp, nextHandler } from "./next-utils";
-import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "./trpc";
-import { inferAsyncReturnType } from "@trpc/server";
-import bodyParser from "body-parser";
-import { IncomingMessage } from "http";
 import { stripeWebhookHandler } from "./webhooks";
+import { PayloadRequest } from "payload/types";
+import { parse } from "url";
 
 const app = express();
 const Port = Number(process.env.PORT) || 3000;
@@ -42,6 +46,37 @@ const start = async () => {
       },
     },
   });
+
+  const cartRouter = express.Router();
+
+  cartRouter.use(payload.authenticate);
+
+  cartRouter.get("/", (req, res) => {
+    const request = req as PayloadRequest;
+
+    if (!request.user) {
+      return res.redirect("/sign-in?origin=cart");
+    }
+
+    const parsedUrl = parse(req.url, true);
+
+    return nextApp.render(req, res, "/cart", parsedUrl.query);
+  });
+
+  app.use("/cart", cartRouter);
+
+  if (process.env.NEXT_BUILD) {
+    app.listen(Port, async () => {
+      payload.logger.info("next.js is building for production");
+
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, "../"));
+
+      process.exit();
+    });
+
+    return;
+  }
 
   app.use(
     "/api/trpc",
